267e270fa6bf01903f49b0ebc99b046dddedb285
Devin Singh <notpublic@gmail.com>
2020-02-24 15:54:33 -0700

    Added ability to search equities

--- LeafPaperTrade/Controllers/View Controllers/SearchViewController.swift
+++ LeafPaperTrade/Controllers/View Controllers/SearchViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class SearchViewController: UIViewController {
     
+    // MARK: - Properties
+    
+    var equties: [Equity] = [] {
+        didSet {
+            //loadViewIfNeeded()
+            self.searchResultTableView.reloadData()
+        }
+    }
+    
     // MARK: - Outlets
     
     @IBOutlet weak var searchResultTableView: UITableView!
@@ -18,30 +27,59 @@ class SearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         searchResultTableView.delegate = self
+        searchResultTableView.dataSource = self
         searchViewSearchBar.delegate = self
     }
     
+    
+    
+    // MARK: - Private Functions
+    func grabEquities(withSearchTerm searchTerm: String) {
+        
+        EquitySearchModelController.getEquities(withSearchTerm: searchTerm) { (result) in
+            switch result {
+            case .success(let equities):
+                DispatchQueue.main.async {
+                    self.equties = equities
+                    
+                }
+            case .failure(let error):
+                print(error.errorDescription ?? error.localizedDescription)
+            }
+        }
+    }
 }
+
+
+// MARK: - UITableViewDataSource Functions
 
 extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return self.equties.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "searchCell") else { return UITableViewCell() }
         
+        cell.textLabel?.text = equties[indexPath.row].symbol
+        
         return cell
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return view.frame.height / 12
+    }
+    
+    
 }
+
+// MARK: - SearchBarDelegate Functions
 
 extension SearchViewController: UISearchBarDelegate {
     
     func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
         self.searchViewSearchBar.layoutIfNeeded()
         self.searchViewSearchBar.setShowsCancelButton(true, animated: true)
-        
     }
     
     func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
@@ -54,8 +92,9 @@ extension SearchViewController: UISearchBarDelegate {
         self.searchViewSearchBar.endEditing(true)
     }
     
-    
-    
-    
-    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        self.searchViewSearchBar.endEditing(true)
+        guard let searchTerm = searchViewSearchBar.text, !searchTerm.isEmpty else { return }
+        grabEquities(withSearchTerm: searchTerm)
+    }
 }
---
+++ LeafPaperTrade/Models/NetworkError.swift
@@ -0,0 +1,26 @@
+//
+//  NetworkError.swift
+//  LeafPaperTrade
+//
+//  Created by Devin Singh on 2/24/20.
+//  Copyright © 2020 Devin Singh. All rights reserved.
+//
+
+import Foundation
+
+enum NetworkError: LocalizedError {
+    case invalidURL
+    case thrownError(Error)
+    case noDataFound
+    
+    var errorDescription: String? {
+        switch self {
+        case .invalidURL:
+            return "Could not contact the server."
+        case .thrownError(let error):
+            return error.localizedDescription
+        case .noDataFound:
+            return "The server responded with no data."
+        }
+    }
+}
